40844ab63586671af0d29dc09a4d880c3063d25e
Rahilsamani <rahilahmed1720@gmail.com>
2024-07-10 06:36:42 +0530

    add navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <div>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,38 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div className="md:p-6 py-2 px-4 flex justify-between items-center">
+      {/* Logo */}
+      <Link to="/">
+        <img src="../assets/logo.png" alt="logo" className="h-10" />
+      </Link>
+
+      {/* Nav Elements */}
+      <div className="font-['Work_Sans'] text-black hidden md:flex items-center gap-10">
+        <Link to="/">
+          <p className="hover:text-gray-600 text-xl cursor-pointer">Home</p>
+        </Link>
+        <Link to="/contact">
+          <p className="hover:text-gray-600 cursor-pointer text-xl">Contact</p>
+        </Link>
+      </div>
+
+      {/* Buttons */}
+      <div className="font-['Work_Sans'] text-black hidden md:flex items-center gap-5">
+        <Link to="/login">
+          <button className="transition duration-500 ease-in-out bg-[#2691CF] rounded-lg px-4 py-1 border-[#2691CF] border-2 hover:bg-transparent text-white hover:text-black">
+            Login
+          </button>
+        </Link>
+        <Link to="/signup">
+          <button className="transition duration-500 ease-in-out bg-[#2691CF] rounded-lg px-4 py-1 border-[#2691CF] border-2 hover:bg-transparent text-white hover:text-black">
+            Sign Up
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
