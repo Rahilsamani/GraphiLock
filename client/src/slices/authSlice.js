65a28a8f32996fba8e9fa73c1a7fece97f9bafba
Rahilsamani <rahilahmed1720@gmail.com>
2024-07-11 06:05:06 +0530

    add redux

---
+++ client/src/slices/authSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  signupData: null,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState: initialState,
+  reducers: {
+    setSignupData(state, value) {
+      state.signupData = value.payload;
+    },
+  },
+});
+
+export const { setSignupData } = authSlice.actions;
+
+export default authSlice.reducer;
